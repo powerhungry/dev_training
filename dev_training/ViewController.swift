2ec84c76c90aa38a38a819e9d75995a450c60a24
StudioMac <studiomac@StudioMacs-iMac.local>
2017-01-29 23:41:41 -0600

    Initial Commit

---
+++ dev_training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev_training
+//
+//  Created by StudioMac on 1/29/17.
+//  Copyright © 2017 StudioMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
